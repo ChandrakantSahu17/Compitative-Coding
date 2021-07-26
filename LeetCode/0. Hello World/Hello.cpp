6fe537b2e14847bfe220b6020f46687e37a41c48
ChandrakantSahu17 <87990448+ChandrakantSahu17@users.noreply.github.com>
2021-07-27 01:42:05 +0530

    New Data

---
+++ LeetCode/0. Hello World/Hello.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main(){
+    cout<<"Hi";
+    return 0;
+}
---
+++ LeetCode/1. Two Sum/1. Two Sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        vector<int> result;
+        for(int i=0; i<nums.size(); i++){
+            if(mp.find(target-nums[i])==mp.end()){
+                mp[nums[i]]=i;
+            }
+            else{
+                result.push_back(mp[target-nums[i]]);
+                result.push_back(i);
+                break;
+            }
+            
+        }
+        return result;
+    }
+};
