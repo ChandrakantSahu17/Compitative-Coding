9bb472efa9100d00134c622b6af930eeb2729afd
ChandrakantSahu17 <87990448+ChandrakantSahu17@users.noreply.github.com>
2021-07-26 23:37:47 +0530

    Create 2. Add Two Numbers.cpp

---
+++ LeetCode/2. Add Two Numbers/2. Add Two Numbers.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     carry += l1->val+l2->val;
+                            l1->val = carry%10;
+                            carry /= 10; : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// Method 1
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry=0;
+        ListNode* l1_copy = l1;
+        if(l1){
+            if(l2){
+                while(l1->next && l2->next){
+                    carry += l1->val+l2->val;
+                    l1->val = carry%10;
+                    carry /= 10;
+                    l1 = l1->next;
+                    l2 = l2->next;
+                }
+                if(l2->next){
+                    l1->next = l2->next;
+                }
+                carry += l1->val+l2->val;
+                l1->val = carry%10;
+                carry /= 10;
+                goto Lable;
+            }
+            else return l1;  
+        }
+        else return l2;
+Lable : if(l1->next){
+            l1=l1->next;
+            while(l1->next){
+                carry += l1->val;
+                l1->val = carry%10;
+                carry /= 10;
+                l1=l1->next;
+            }
+            carry += l1->val;
+            l1->val = carry%10;
+            carry /= 10;
+        }                   
+        if(carry){
+            l1->next = new ListNode(carry);
+        }
+        return l1_copy;  
+    }
+};
+
+/*
+Runtime: 36 ms, faster than 39.81% of C++ online submissions for Add Two Numbers.
+Memory Usage: 70.8 MB, less than "99.58%" of C++ online submissions for Add Two Numbers.
+
+But this is not a good way to code because at some point if we need to access the original 
+link list we will be accessing the modified one.
+*/
