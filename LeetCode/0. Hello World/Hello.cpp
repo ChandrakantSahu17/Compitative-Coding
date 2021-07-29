7b8859b08d0740d399a2d15e2d2a190793062154
Chandrakant Sahu <87990448+ChandrakantSahu17@users.noreply.github.com>
2021-07-29 19:50:10 +0530

    C++ working
    C++ is working fine in VS code #ubuntu

--- LeetCode/0. Hello World/Hello.cpp
+++ LeetCode/0. Hello World/Hello.cpp
@@ -1,9 +1,10 @@
 // The aim of this file is to check proper working of VS Code editor
 
 #include<iostream>
+using namespace std;
 
 int main(){
-    int x=10;
-    std::cout<<"Hi"<<x;
+    int x=11;
+    cout<<"Hello "<<x<<endl;
     return 0;
 }
